23a104e48e918d86f6e7c192ec67584ff7460278
SJDwebtechzone <connectwithdevspectra@gmail.com>
2025-11-11 08:54:54 +0000

    Stop tracking collectstatic output; add source images and code changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key')
 
-ALLOWED_HOSTS = ['devspectra.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['devspectra.in', 'www.devspectra.in']
 
 # Application definition
 
